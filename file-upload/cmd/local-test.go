e8ec61eefb8bcda4baa23a47e96568f9f1ce0629
KNU-K <knukang334@gmail.com>
2025-03-19 15:34:46 +0900

    feat : json 형식의 반환 테스트

--- file-upload/cmd/local-test.go
+++ file-upload/cmd/local-test.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", function.HelloWorld)
+	// http.HandleFunc("/file-upload", function.FileUpload)
+	http.HandleFunc("/hello", function.HelloWorld)
 	fmt.Println("starting server")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
         fmt.Println(err)
--- file-upload/main.go
+++ file-upload/main.go
@@ -1,14 +1,28 @@
 package function
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
+type Hello struct{
+	Message string `json:"message"`
+}
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!2333")
+
+	response:= Hello{Message: "Hello, World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
+
+// func FileUpload(w http.ResponseWriter, r *http.Request){
+// 	if r.Method != http.MethodPost {
+// 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+// 		return
+// 	}
+// 	fmt.Fprint(w, "File uploaded successfully!")
+// }
