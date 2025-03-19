f68128c351ebb1832a1c53eb5b79c9729831b62a
KNU-K <knukang334@gmail.com>
2025-03-20 04:52:11 +0900

    feat: 파일 업로드 기능 개선 및 비동기 처리 추가
    - go루틴을 추가해서 파일 전송 부분 비동기 처리
    
    - 파일 이름 랜덤하게 변경
    
    - 결정론적인 파일 url 을 미리 리턴턴

--- file-upload/handler.go
+++ file-upload/handler.go
@@ -5,67 +5,76 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"cloud.google.com/go/storage"
 )
 
-type UploadResponse struct {
-    Message string `json:"message"`
-    URL     string `json:"url"`
+const bucketName = "fitus-file-bucket"
+
+var storageClient *storage.Client
+
+func init() {
+	ctx := context.Background()
+	var err error
+	storageClient, err = storage.NewClient(ctx)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create storage client: %v", err))
+	}
 }
 
-const (
-	bucketName = "fitus-file-bucket"
-)
+func generateFileName() string {
+	return fmt.Sprintf("%d-%d", time.Now().UnixNano(), rand.Intn(1000000))
+}
 
 func FileUpload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	// Parse multipart form (최대 10MB)
+
 	if err := r.ParseMultipartForm(10 << 20); err != nil {
 		http.Error(w, "Error Parsing Form", http.StatusBadRequest)
 		return
 	}
-	// "file" 필드에서 파일 추출
-	file, header, err := r.FormFile("file")
+
+	file, _, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Error Retrieving the File", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	// GCS 클라이언트 생성
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	fileName := generateFileName()
+	url := fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, fileName)
+
+	go func() {
+		if err := uploadFileToGCS(file, fileName); err != nil {
+			fmt.Printf("Error uploading file to GCS: %v\n", err)
+		} else {
+			fmt.Println("File uploaded successfully to GCS!")
+		}
+	}()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "File uploaded successfully to GCS!",
+		"url":     url,
+	})
+}
+
+func uploadFileToGCS(file io.Reader, fileName string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
 	defer cancel()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		http.Error(w, "Error creating storage client", http.StatusInternalServerError)
-		return
-	}
-	defer client.Close()
 
-	// 업로드할 객체 생성 (파일명 사용)
-	object := client.Bucket(bucketName).Object(header.Filename)
-	wc := object.NewWriter(ctx)
-	// 파일 데이터를 객체에 복사
+	wc := storageClient.Bucket(bucketName).Object(fileName).NewWriter(ctx)
+	defer wc.Close()
+
 	if _, err := io.Copy(wc, file); err != nil {
-		http.Error(w, "Error uploading file", http.StatusInternalServerError)
-		return
+		return err
 	}
-	if err := wc.Close(); err != nil {
-		http.Error(w, "Error closing writer", http.StatusInternalServerError)
-		return
-	}
-	url := fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, header.Filename)
 
-    w.Header().Set("Content-Type", "application/json")
-    response := UploadResponse{
-        Message: "File uploaded successfully to GCS!",
-        URL:     url,
-    }
-    json.NewEncoder(w).Encode(response)
+	return nil
 }
