0c8d1e16237553be781a2e7bc3263ef2c4b87665
KNU-K <knukang334@gmail.com>
2025-03-20 04:51:22 +0900

    fix: 파일 업로드 핸들러 추가 및 서버 시작 로직 구현(로컬 테스트)

--- file-upload/cmd/main.go
+++ file-upload/cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	function "github.com/fit-us/be-generic-system"
+)
+
 func main() {
-	// 	// http.HandleFunc("/file-upload", function.FileUpload)
-	// 	// http.HandleFunc("/hello", function.HelloWorld)
-	// 	// fmt.Println("starting server")
-	// 	// if err := http.ListenAndServe(":8080", nil); err != nil {
-	//     //     fmt.Println(err)
-	//     // }
+	http.HandleFunc("/file-upload", function.FileUpload)
+	fmt.Println("starting server")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
