c97efbfec3b2205fe43da546026f8557ca6e1e62
KNU-K <knukang334@gmail.com>
2025-03-20 04:15:11 +0900

    feat: local-test.go 파일 제거 및 main.go 파일 생성

---
+++ file-upload/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// 	// http.HandleFunc("/file-upload", function.FileUpload)
+	// 	// http.HandleFunc("/hello", function.HelloWorld)
+	// 	// fmt.Println("starting server")
+	// 	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	//     //     fmt.Println(err)
+	//     // }
+}
