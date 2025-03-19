9d847ef6865645e9560a8dc7a952144b0c1229b3
KNU-K <knukang334@gmail.com>
2025-03-20 04:15:05 +0900

    feat: 파일 업로드 기능 구현 및 기존 main.go 파일 제거

---
+++ file-upload/handler.go
@@ -0,0 +1,71 @@
+package function
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+type UploadResponse struct {
+    Message string `json:"message"`
+    URL     string `json:"url"`
+}
+
+const (
+	bucketName = "fitus-file-bucket"
+)
+
+func FileUpload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// Parse multipart form (최대 10MB)
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "Error Parsing Form", http.StatusBadRequest)
+		return
+	}
+	// "file" 필드에서 파일 추출
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Error Retrieving the File", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// GCS 클라이언트 생성
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	defer cancel()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		http.Error(w, "Error creating storage client", http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
+
+	// 업로드할 객체 생성 (파일명 사용)
+	object := client.Bucket(bucketName).Object(header.Filename)
+	wc := object.NewWriter(ctx)
+	// 파일 데이터를 객체에 복사
+	if _, err := io.Copy(wc, file); err != nil {
+		http.Error(w, "Error uploading file", http.StatusInternalServerError)
+		return
+	}
+	if err := wc.Close(); err != nil {
+		http.Error(w, "Error closing writer", http.StatusInternalServerError)
+		return
+	}
+	url := fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, header.Filename)
+
+    w.Header().Set("Content-Type", "application/json")
+    response := UploadResponse{
+        Message: "File uploaded successfully to GCS!",
+        URL:     url,
+    }
+    json.NewEncoder(w).Encode(response)
+}
